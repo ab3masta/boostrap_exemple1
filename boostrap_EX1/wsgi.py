4ac04f37fc120313ff9cf72a47f31b83f892bc66
AbdouAbarchiAboubacar <abdouabarchia@gmail.com>
2019-11-24 17:16:27 +0100

    commit

---
+++ boostrap_EX1/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for boostrap_EX1 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boostrap_EX1.settings')
+
+application = get_wsgi_application()
